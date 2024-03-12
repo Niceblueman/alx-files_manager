0d7a2c7079dd84d13150adc5642807a23c9a0fc1
Niceblueman <wintheiser761@gmail.com>
2024-03-12 00:34:55 +0000

    - back to express

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
-import Fastify from 'fastify';
 import express from 'express';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import dotenv from 'dotenv';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Fastifyexpress from '@fastify/express';
 import router from './routes/index';
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
 const port = parseInt(process.env.PORT, 10) || 5000;
 
-const fastify = Fastify({
-  logger: process.env.NODE_ENV !== 'production',
+const app = express();
+
+app.use(express.json());
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
-async function build() {
-  await fastify.register(Fastifyexpress);
-  fastify.use(express.json());
-  fastify.use('/', router);
-  fastify.express.disabled('x-powered-by');
-  return fastify;
-}
-build()
-  .then((fastify) => fastify.listen({ port }, (err, address) => {
-    console.log(`server running on ${address}`);
-    if (err) {
-      throw err;
-    }
-  }))
-  .catch(console.log);
-export default fastify;
+
+export default app;
